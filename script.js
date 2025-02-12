7d3517174ef562b934af31f18d3836a846ac66f6
saaralehtoviita1@gmail.com <saaralehtoviita1@gmail.com>
2025-02-12 13:48:55 +0200

    testailua

--- script.js
+++ script.js
@@ -5,23 +5,22 @@ fetch('https://saaralehtoviita.github.io/digitekniikat/tiedot.JSON')
 .then(function(tiedot) {
     let placeholder = document.querySelector("#data-output");
     let out = "";
-    for(let tieto of tiedot) {
+
         out += `
         <tr> 
-            <td>${tieto.otsikko}</td>
-            <td>${tieto.kuvaus}</td>
-            <td>${tieto.nimi}</td>
-            <td>${tieto.tunnus}</td>
-            <td>${tieto.opintopisteet}</td>
-            <td>${tieto.sisalto.join(', ')}</td>
+            <td>${tiedot.otsikko}</td>
+            <td>${tiedot.kuvaus}</td>
+            <td>${tiedot.nimi}</td>
+            <td>${tiedot.tunnus}</td>
+            <td>${tiedot.opintopisteet}</td>
+            <td>${tiedot.sisalto.join(', ')}</td>
             <td>
-                ${tieto.tekniikat.map(tekniikka => `<a href="${tekniikka.linkki}">${tekniikka.aihe}</a>`).join(', ')}
+                ${tiedot.tekniikat.map(tekniikka => `<a href="${tekniikka.linkki}">${tekniikka.aihe}</a>`).join(', ')}
             </td>
         </tr>
         `;
-    }
-    placeholder.innerHTML = out;
-}) 
+        placeholder.innerHTML = out;
+    })
 .catch(function(error) {
     document.getElementById("data-output").innerHTML = "<tr><td colspan='7'>Tietoa ei pystytä hakemaan</td></tr>";
 });
