e474ac67db3c8901dc149831c08f48d1342a3ede
SaaraLehtoviita <saaralehtoviita1@gmail.com>
2025-09-10 12:31:27 +0300

    about sivua tyylitelty, contact-sivulle yhteydenottolomake (ei vielä toimi, tarvitaan Maarilta emailJS käyttäjätiedot

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,44 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+emailjs.init('YOUR_PUBLIC_KEY'); //sulkujen sisään tulee Maarin Public Key
+
+//tehdään muuttuja lomakkeen tiedoista, etsii id:n contact-form html-tiedostosta
+const form = document.getElementById('contact-form');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();  // estää sivun uudelleenlatauksen
+
+    //kenttien arvot
+    const name = form.elements['user_name'].value.trim();
+    const email = form.elements['user_email'].value.trim();
+    const message = form.elements['message'].value.trim();
+
+    //tarkastetaan, ettei ole tyhjiä kenttiä
+    if (!name || !email || !message) {
+        alert('Please fill in all fields so that I can get in touch.')
+        return;
+    }
+
+    //sähköpostin tarkastus
+    if (!email.includes('@') || !email.includes('.')) {
+        alert('Please enter a valid email adress');
+        return;
+    }
+
+//funktio, joka lähettää lomakkeen tiedot EmailJS:n kautta
+//YOUR_SERVICE_ID = EmailJS:n palvelutunnus (Service ID
+//YOUR_TEMPLATE_ID = EmailJS:n mallipohjan (Template ID) tunnus
+//this viittaa lähetettävän lomakkeen tietoihin
+
+emailjs.sendform('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', this, 'YOUR_PUBLIC_KEY')
+    .then(() => {
+        alert('Message sent successfully!');
+        form.reset();
+    }, (error) => {
+        console.error(error);
+        alert('Failed to send message. Please try again</span>');
+    });
+});
+
